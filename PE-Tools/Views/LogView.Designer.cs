c7f71e100b0c955e1e409a58f103ccfc40feee7c
Andy <andy.tomlinson@practiceevolve.com>
2023-06-12 16:47:53 +0100

    Completed basic log viewer with search

--- PE-Tools/Views/LogView.Designer.cs
+++ PE-Tools/Views/LogView.Designer.cs
@@ -28,22 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.userControlProjectSelector1 = new PE_Tools.Views.UserControlProjectSelector();
             this.titleLabelControl = new DevExpress.XtraEditors.LabelControl();
             this.outputListBox = new System.Windows.Forms.ListBox();
             this.labelSelectLog = new DevExpress.XtraEditors.LabelControl();
-            this.cbFolders = new System.Windows.Forms.ComboBox();
+            this.cbFiles = new System.Windows.Forms.ComboBox();
+            this.labelSearch = new DevExpress.XtraEditors.LabelControl();
+            this.textEdit1 = new DevExpress.XtraEditors.TextEdit();
+            this.buttonNext = new System.Windows.Forms.Button();
+            this.buttonPrevious = new System.Windows.Forms.Button();
+            this.labelSearchSummary = new DevExpress.XtraEditors.LabelControl();
+            this.userControlProjectSelector1 = new PE_Tools.Views.UserControlProjectSelector();
+            this.buttonFirst = new System.Windows.Forms.Button();
+            this.buttonLast = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).BeginInit();
             this.SuspendLayout();
-            // 
-            // userControlProjectSelector1
-            // 
-            this.userControlProjectSelector1.Callback = null;
-            this.userControlProjectSelector1.FolderNames = null;
-            this.userControlProjectSelector1.Location = new System.Drawing.Point(14, 37);
-            this.userControlProjectSelector1.Name = "userControlProjectSelector1";
-            this.userControlProjectSelector1.SelectedFolder = null;
-            this.userControlProjectSelector1.Size = new System.Drawing.Size(395, 71);
-            this.userControlProjectSelector1.TabIndex = 44;
             // 
             // titleLabelControl
             // 
@@ -71,32 +69,127 @@
             // 
             this.labelSelectLog.Appearance.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.labelSelectLog.Appearance.Options.UseFont = true;
-            this.labelSelectLog.Location = new System.Drawing.Point(422, 52);
+            this.labelSelectLog.Location = new System.Drawing.Point(23, 50);
             this.labelSelectLog.Name = "labelSelectLog";
             this.labelSelectLog.Size = new System.Drawing.Size(62, 16);
             this.labelSelectLog.TabIndex = 49;
             this.labelSelectLog.Text = "Select Log";
             // 
-            // cbFolders
+            // cbFiles
             // 
-            this.cbFolders.FormattingEnabled = true;
-            this.cbFolders.Location = new System.Drawing.Point(422, 74);
-            this.cbFolders.Name = "cbFolders";
-            this.cbFolders.Size = new System.Drawing.Size(364, 21);
-            this.cbFolders.TabIndex = 50;
-            this.cbFolders.SelectedIndexChanged += new System.EventHandler(this.cbFolders_SelectedIndexChanged);
+            this.cbFiles.FormattingEnabled = true;
+            this.cbFiles.Location = new System.Drawing.Point(111, 49);
+            this.cbFiles.Name = "cbFiles";
+            this.cbFiles.Size = new System.Drawing.Size(364, 21);
+            this.cbFiles.TabIndex = 50;
+            this.cbFiles.SelectedIndexChanged += new System.EventHandler(this.cbFiles_SelectedIndexChanged);
+            // 
+            // labelSearch
+            // 
+            this.labelSearch.Appearance.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelSearch.Appearance.Options.UseFont = true;
+            this.labelSearch.Location = new System.Drawing.Point(23, 77);
+            this.labelSearch.Name = "labelSearch";
+            this.labelSearch.Size = new System.Drawing.Size(74, 16);
+            this.labelSearch.TabIndex = 51;
+            this.labelSearch.Text = "Search Term";
+            // 
+            // textEdit1
+            // 
+            this.textEdit1.Location = new System.Drawing.Point(111, 76);
+            this.textEdit1.Name = "textEdit1";
+            this.textEdit1.Size = new System.Drawing.Size(196, 20);
+            this.textEdit1.TabIndex = 52;
+            this.textEdit1.EditValueChanged += new System.EventHandler(this.textEdit1_EditValueChanged);
+            // 
+            // buttonNext
+            // 
+            this.buttonNext.Enabled = false;
+            this.buttonNext.Location = new System.Drawing.Point(844, 72);
+            this.buttonNext.Name = "buttonNext";
+            this.buttonNext.Size = new System.Drawing.Size(75, 23);
+            this.buttonNext.TabIndex = 53;
+            this.buttonNext.Text = "Next";
+            this.buttonNext.UseVisualStyleBackColor = true;
+            this.buttonNext.Click += new System.EventHandler(this.buttonNext_Click);
+            // 
+            // buttonPrevious
+            // 
+            this.buttonPrevious.Enabled = false;
+            this.buttonPrevious.Location = new System.Drawing.Point(763, 72);
+            this.buttonPrevious.Name = "buttonPrevious";
+            this.buttonPrevious.Size = new System.Drawing.Size(75, 23);
+            this.buttonPrevious.TabIndex = 54;
+            this.buttonPrevious.Text = "Previous";
+            this.buttonPrevious.UseVisualStyleBackColor = true;
+            this.buttonPrevious.Click += new System.EventHandler(this.buttonPrevious_Click);
+            // 
+            // labelSearchSummary
+            // 
+            this.labelSearchSummary.Appearance.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelSearchSummary.Appearance.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.labelSearchSummary.Appearance.Options.UseFont = true;
+            this.labelSearchSummary.Appearance.Options.UseForeColor = true;
+            this.labelSearchSummary.Location = new System.Drawing.Point(332, 77);
+            this.labelSearchSummary.Name = "labelSearchSummary";
+            this.labelSearchSummary.Size = new System.Drawing.Size(94, 16);
+            this.labelSearchSummary.TabIndex = 55;
+            this.labelSearchSummary.Text = "add search term";
+            // 
+            // userControlProjectSelector1
+            // 
+            this.userControlProjectSelector1.Callback = null;
+            this.userControlProjectSelector1.FolderNames = null;
+            this.userControlProjectSelector1.Location = new System.Drawing.Point(650, 3);
+            this.userControlProjectSelector1.Name = "userControlProjectSelector1";
+            this.userControlProjectSelector1.SelectedFolder = null;
+            this.userControlProjectSelector1.Size = new System.Drawing.Size(395, 71);
+            this.userControlProjectSelector1.TabIndex = 44;
+            this.userControlProjectSelector1.Visible = false;
+            // 
+            // buttonFirst
+            // 
+            this.buttonFirst.Enabled = false;
+            this.buttonFirst.Location = new System.Drawing.Point(663, 72);
+            this.buttonFirst.Name = "buttonFirst";
+            this.buttonFirst.Size = new System.Drawing.Size(75, 23);
+            this.buttonFirst.TabIndex = 56;
+            this.buttonFirst.Text = "First";
+            this.buttonFirst.UseVisualStyleBackColor = true;
+            this.buttonFirst.Click += new System.EventHandler(this.buttonFirst_Click);
+            this.buttonFirst.Enabled = false;
+            // 
+            // buttonLast
+            // 
+            this.buttonLast.Enabled = false;
+            this.buttonLast.Location = new System.Drawing.Point(953, 72);
+            this.buttonLast.Name = "buttonLast";
+            this.buttonLast.Size = new System.Drawing.Size(75, 23);
+            this.buttonLast.TabIndex = 57;
+            this.buttonLast.Text = "Last";
+            this.buttonLast.UseVisualStyleBackColor = true;
+            this.buttonLast.Click += new System.EventHandler(this.buttonLast_Click);
+            this.buttonLast.Enabled = false;
             // 
             // LogView
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.cbFolders);
+            this.Controls.Add(this.buttonLast);
+            this.Controls.Add(this.buttonFirst);
+            this.Controls.Add(this.labelSearchSummary);
+            this.Controls.Add(this.buttonPrevious);
+            this.Controls.Add(this.buttonNext);
+            this.Controls.Add(this.textEdit1);
+            this.Controls.Add(this.labelSearch);
+            this.Controls.Add(this.cbFiles);
             this.Controls.Add(this.labelSelectLog);
             this.Controls.Add(this.outputListBox);
             this.Controls.Add(this.userControlProjectSelector1);
             this.Controls.Add(this.titleLabelControl);
             this.Name = "LogView";
             this.Size = new System.Drawing.Size(1145, 393);
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -108,6 +201,13 @@
         private DevExpress.XtraEditors.LabelControl titleLabelControl;
         private System.Windows.Forms.ListBox outputListBox;
         private DevExpress.XtraEditors.LabelControl labelSelectLog;
-        private System.Windows.Forms.ComboBox cbFolders;
+        private System.Windows.Forms.ComboBox cbFiles;
+        private DevExpress.XtraEditors.LabelControl labelSearch;
+        private DevExpress.XtraEditors.TextEdit textEdit1;
+        private System.Windows.Forms.Button buttonNext;
+        private System.Windows.Forms.Button buttonPrevious;
+        private DevExpress.XtraEditors.LabelControl labelSearchSummary;
+        private System.Windows.Forms.Button buttonFirst;
+        private System.Windows.Forms.Button buttonLast;
     }
 }
--- PE-Tools/Views/LogView.cs
+++ PE-Tools/Views/LogView.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using DevExpress.Data.Svg;
+using PE_Tools.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,14 +16,169 @@ namespace PE_Tools.Views
 {
     public partial class LogView : UserControl
     {
+        public delegate void FolderSelectedNotification();
+        public List<string> FileNames { get; set; }
+        public Folder SelectedFile{ get; set; }
+        public FolderSelectedNotification Callback { get; set; }
+        public List<string> LogData { get; set; }
+        private Dictionary<int, string> SearchResults = new Dictionary<int, string>();
+        private int selectedSearchItem = 0;
+
         public LogView()
         {
             InitializeComponent();
+            this.LogData = new List<string>();
+            this.outputListBox.DataSource = this.LogData;
+            var files = getFiles();
+            this.cbFiles.DataSource = files;
+            this.cbFiles.ValueMember = "ID";
+            this.cbFiles.DisplayMember = "FullPath";
+            this.cbFiles.SelectedIndex = 0;
         }
 
-        private void cbFolders_SelectedIndexChanged(object sender, EventArgs e)
-        {
 
+        private List<Folder> getFiles()
+        {
+            var folderName = ConfigurationSettings.AppSettings["logFolder"];
+            if (!string.IsNullOrEmpty(folderName))
+            {
+                FileNames = new List<string> { $"{folderName}\\CMSService.log", $"{folderName}\\Client.log" };
+
+                if (FileNames != null && FileNames.Any())
+                {
+                    var files = new List<Folder>()
+                    {
+                        new Folder(@"select")
+                    };
+                    FileNames.ForEach(f => files.Add(new Folder(f)));
+                    return files;
+                }
+            }
+
+            return new List<Folder>() {
+                new Folder(@"no folders identified in config file") 
+            };
+        }
+
+        private void cbFiles_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cbFiles.SelectedIndex < 1)
+            {
+                this.SelectedFile = null;
+                return;
+            }
+            this.outputListBox.DataSource = null;
+            this.SelectedFile = this.cbFiles.SelectedItem as Folder;
+            using (var fs = File.Open(this.SelectedFile.FullPath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            using (var sr = new StreamReader(fs, Encoding.Default))
+            {
+                string line;
+
+                this.LogData.Clear();
+
+                while ((line = sr.ReadLine()) != null)
+                {
+                    this.LogData.Add(line);
+                }
+            }
+
+            this.outputListBox.DataSource = this.LogData;
+
+            if (this.Callback != null)
+            {
+                Callback();
+            }
+        }
+
+        private void textEdit1_EditValueChanged(object sender, EventArgs e)
+        {
+            var searchTerm = textEdit1.Text;
+            if (!string.IsNullOrWhiteSpace(searchTerm) && searchTerm.Count() >= 3)
+            {
+                search(searchTerm);
+            }
+            else
+            {
+                this.SearchResults.Clear();
+                showSelectedItem();
+            }
+        }
+
+        private void search(string searchTerm)
+        {
+            this.SearchResults.Clear();
+            var entries = this.LogData.Where(d => d.Contains(searchTerm)).ToList();
+            entries.ForEach(e =>
+            {
+                this.SearchResults[LogData.IndexOf(e)] = e;
+            });
+
+            if(entries.Any())
+            {
+                this.selectedSearchItem = 0;
+                showSelectedItem();
+            }
+            else
+            {
+                this.selectedSearchItem = 0;
+                showSelectedItem();
+            }
+        }
+
+        private void buttonPrevious_Click(object sender, EventArgs e)
+        {
+            if(selectedSearchItem > 0)
+            {
+                selectedSearchItem--;
+                showSelectedItem();
+            }
+        }
+
+        private void buttonNext_Click(object sender, EventArgs e)
+        {
+            if(selectedSearchItem < SearchResults.Count() - 1)
+            {
+                selectedSearchItem++;
+                showSelectedItem();
+            }
+        }
+
+        private void showSelectedItem()
+        {
+            if (SearchResults.Any())
+            {
+                var index = SearchResults.Keys.ToList()[selectedSearchItem];
+                var nthValue = SearchResults[index];
+                this.outputListBox.SelectedIndex = index;
+                this.outputListBox.TopIndex = outputListBox.SelectedIndex;
+                this.labelSearchSummary.Text = $"Showing result {selectedSearchItem + 1} of {SearchResults.Count}";
+                this.buttonPrevious.Enabled = selectedSearchItem > 0;
+                this.buttonNext.Enabled = selectedSearchItem < SearchResults.Count() - 1;
+                this.buttonFirst.Enabled = this.buttonLast.Enabled = SearchResults.Any();
+            }
+            else
+            {
+                this.outputListBox.SelectedIndex = 0;
+                this.outputListBox.TopIndex = outputListBox.SelectedIndex;
+                this.labelSearchSummary.Text = "add search term";
+                this.buttonPrevious.Enabled =
+                this.buttonNext.Enabled =
+                this.buttonFirst.Enabled = 
+                this.buttonLast.Enabled = false; ;
+
+            }
+        }
+
+        private void buttonLast_Click(object sender, EventArgs e)
+        {
+            selectedSearchItem = SearchResults.Count() - 1;
+            showSelectedItem();
+        }
+
+        private void buttonFirst_Click(object sender, EventArgs e)
+        {
+            selectedSearchItem = 0;
+            showSelectedItem();
         }
     }
 }
